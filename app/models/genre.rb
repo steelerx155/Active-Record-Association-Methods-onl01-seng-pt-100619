ef18f31dceb85a5b3ae51cac28032a3b501768bd
Randy Steele <steelerx155@gmail.com>
2019-12-26 17:55:43 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,5 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
      self.artists.map(&:name)
-    # self.artists(&:name)
   end
 end
